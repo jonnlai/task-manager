8037d40c2b5345706574365ea12ac002f0c67bb5
Jonna Laine <jonna.jp.laine@gmail.com>
2024-07-08 19:31:32 +0000

    add functionality to view the tasks

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -13,4 +13,8 @@ document.addEventListener('DOMContentLoaded', function() {
       let selects = document.querySelectorAll('select');
       M.FormSelect.init(selects);
 
+      //collapsible initializatio
+      let collapsibles = document.querySelectorAll('.collapsible');
+      M.Collapsible.init(collapsibles);
+
   });
